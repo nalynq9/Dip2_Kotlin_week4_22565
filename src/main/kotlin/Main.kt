e40ca7825da1c2a969749b4fc0db21eecc82ffac
nalynq9 <janisdinosaur@gmail.com>
2022-11-08 17:08:49 +0700

    initial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    //testWhen();
+    //testWhen2()
+    //testWhen3()
+    //testWhen4()
+    //testWhen5()
+    //testForInLoop()
+    //testForInLoop2()
+    //testForInLoop3()
+    //testForInLoop4()
+    //testForInLoop5()
+    //testForLoopArray()
+    //testForLoopString()
+    //testWhileLoop()
+    //testDoWhile()
+    //testBreakExpress()
+    testBreakExpress2()
+}
